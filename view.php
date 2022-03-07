2d1857fd0e8a94022b557616d660db440ed44268
Magali Fabri <magali_90@live.nl>
2022-03-07 09:27:09 +0100

    Add <form> for user to submit a word

--- view.php
+++ view.php
@@ -1,13 +1,19 @@
 <!doctype html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport"
-		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Game</title>
 </head>
+
 <body>
 	<!-- TODO: add a form for the user to play the game -->
+	<form action="" method="POST">
+		<label for="word">enter word</label>
+		<input id="word" type="text" name="word">
+	</form>
 </body>
+
 </html>
