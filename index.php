21f0dd9ee4fe2059e790ec68dd11f8ed2ceafed2
Magali Fabri <magali_90@live.nl>
2022-03-07 12:21:39 +0100

    Enable Player class

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Require the correct variable type to be used (no auto-converting)
-declare(strict_types = 1);
+declare(strict_types=1);
 
 // Show errors so we get helpful information
 ini_set('display_errors', '1');
@@ -11,7 +11,7 @@ error_reporting(E_ALL);
 // Load your classes
 require_once 'classes/Data.php';
 require_once 'classes/LanguageGame.php';
-// require_once 'classes/Player.php'; // Only needed for extra's
+require_once 'classes/Player.php'; // Only needed for extra's
 require_once 'classes/Word.php';
 
 // Start the game
