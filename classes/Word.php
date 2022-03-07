586cd2583b91f307d3811267b6520fc6b6aa60d5
Magali Fabri <magali_90@live.nl>
2022-03-07 11:56:25 +0100

    Add basic verification method in Word

--- classes/Word.php
+++ classes/Word.php
@@ -15,6 +15,12 @@ class Word
     public function verify(string $answer): bool
     {
         // TODO: use this function to verify if the provided answer by the user matches the correct one
+        if ($answer === $this->answer) {
+            return true;
+        } else {
+            return false;
+        }
+
         // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
         // Bonus (hard): can you allow answers with small typo's (max one character different)?
     }
