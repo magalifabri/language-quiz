10442cb86c6ee4466174355c3c72aa173dcca021
Magali Fabri <magali_90@live.nl>
2022-03-07 11:57:13 +0100

    Change random word selection and storage

--- classes/LanguageGame.php
+++ classes/LanguageGame.php
@@ -1,9 +1,10 @@
 <?php
 
+session_start();
+
 class LanguageGame
 {
     private array $words;
-    private Word $wordToTranslate;
 
     public function __construct()
     {
@@ -16,8 +17,6 @@ class LanguageGame
             // TODO: create instances of the Word class to be added to the words array
             array_push($this->words, new Word($englishTranslation, $frenchTranslation));
         }
-
-        $this->wordToTranslate = $this->words[rand(0, count($this->words) - 1)];
     }
 
     public function run(): void
@@ -26,20 +25,46 @@ class LanguageGame
 
         // Option A: user visits site first time (or wants a new word)
         // TODO: select a random word for the user to translate
-        $wordToTranslate = $this->words[rand(0, count($this->words) - 1)];
+        if (empty($_SESSION['wordIndex'])) {
+            $this->selectRandomWord();
+        }
 
         // Option B: user has just submitted an answer
         // TODO: verify the answer (use the verify function in the word class) - you'll need to get the used word from the array first
-        if (!empty($_POST['word'])) {
-            $givenAnswer = $_POST['word'];
-        }
+        // if (!empty($_POST['word'])) {
+        //     $givenAnswer = $_POST['word'];
+        //     $selectedWord = $this->words[$_SESSION['wordIndex']];
+
+        //     $correct = $selectedWord->verify($givenAnswer);
+
+        //     if ($correct) {
+        //         echo 'Correct!'
+        //             . '<br>'
+        //             . '<b><i>' . $givenAnswer . '</i></b>' . ' (FR) is ' . '<b><i>' . $selectedWord->word . '</i></b>' . ' (EN).'
+        //             . '<br>'
+        //             . 'New word selected.';
+
+        //         $this->selectRandomWord();
+        //     } else {
+        //         echo 'Wrong!'
+        //             . '<br>'
+        //             . '<b><i>' . $givenAnswer . '</i></b>' . ' (FR) is not ' . '<b><i>' . $selectedWord->word . '</i></b>' . ' (EN).'
+        //             . '<br>'
+        //             . 'Try again.';
+        //     }
+        // }
 
         // TODO: generate a message for the user that can be shown
 
     }
 
+    public function selectRandomWord()
+    {
+        $_SESSION['wordIndex'] = rand(0, count($this->words) - 1);
+    }
+
     public function getWordToTranslate()
     {
-        return $this->wordToTranslate->word;
+        return $this->words[$_SESSION['wordIndex']]->word;
     }
 }
