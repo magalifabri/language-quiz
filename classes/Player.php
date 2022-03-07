5bca1bd85990e4b2c2c974879c2ef95682171d74
Magali Fabri <magali_90@live.nl>
2022-03-07 12:21:08 +0100

    Create basic Player class

--- classes/Player.php
+++ classes/Player.php
@@ -3,9 +3,18 @@
 class Player
 {
     // TODO: add name and score
+    public string $name;
+    public int $score;
 
-    public function __construct()
+    public function __construct($name)
     {
         // TODO: add 👤 automatically to their name
+        $this->setName($name);
+        $this->score = 0;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name . ' 👤';
     }
 }
