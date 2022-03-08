6eb58c932c5a35e9e4d4c7c09ea7b58d3001b08e
Magali Fabri <magali_90@live.nl>
2022-03-08 07:52:06 +0100

    Tweak layout and styling

--- view.php
+++ view.php
@@ -46,15 +46,24 @@
 		<div class="top-bar">
 			<p>Hello <?= $game->player->name ?></p>
 			<p><?= $game->player->score ?> √ - <?= $game->player->errors ?> ×</p>
+			<form action="" method="POST">
+				<button name="reset">reset</button>
+			</form>
 		</div>
 
 		<hr>
 
-		<!-- <p class="translate-section-h">Translate: English to French</p> -->
-		<div class="translate-section">
-			<p class="word-to-translate">English: <span><?= $game->getWordToTranslate() ?></span></p>
+		<div class="from">
+			<p class="language">English:</p>
+			<p class="word-to-translate"><?= $game->getWordToTranslate() ?></p>
 			<form action="" method="POST">
-				<label for="word">French: </label>
+				<button name="pass">pass</button>
+			</form>
+		</div>
+
+		<div class="to">
+			<p class="language">French:</p>
+			<form action="" method="POST" class="translation-input-form">
 				<input id="word" type="text" name="word" placeholder="enter translation">
 			</form>
 		</div>
@@ -62,17 +71,6 @@
 		<?php if (!empty($game->userFeedback)) : ?>
 			<p class="user-feedback"><?= $game->userFeedback ?></p>
 		<?php endif ?>
-
-		<div class="buttons">
-			<form action="" method="POST">
-				<button name="pass">pass</button>
-			</form>
-
-
-			<form action="" method="POST">
-				<button name="reset">reset</button>
-			</form>
-		</div>
 
 	<?php endif ?>
 
